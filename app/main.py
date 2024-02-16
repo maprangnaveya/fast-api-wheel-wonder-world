516ac7503f8819b66923df25ae82c2353edbff27
Naveya Jintaya <naveya@divertise.asis>
2024-02-17 01:26:08 +0700

    Be able to connect to MongoDB in docker.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-app = FastAPI()
+from .config import Settings
+from .utils import init_mongo
+
+settings = Settings()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # start up
+    app.state.mongo_client, app.state.mongo_db = await init_mongo(
+        settings.mongo_db,
+        settings.mongodb_url,
+    )
+    yield
+    # shutdown
+
+
+app = FastAPI(title=settings.app_name, lifespan=lifespan)
 
 
 @app.get("/")
