0588548af96c2198c057e9bfe7c5d060f334389c
Naveya Jintaya <naveya@divertise.asis>
2024-02-17 22:25:54 +0700

    create function check and change password to UserForDB.

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,7 @@
 from pydantic import Field, EmailStr
 
+from core.security import generate_salt, get_password_hash, verify_password
+
 from .db_model import DBModelMixin
 from .read_write_model import RWModel
 
@@ -12,6 +14,15 @@ class BaseUser(RWModel):
 class UserForDB(DBModelMixin, BaseUser):
     salt: str = ""
     hashed_password: str = ""
+
+    def check_password(self, password: str):
+        return verify_password(self.salt + password, self.hashed_password)
+
+    def change_password(self, password: str):
+        self.salt = generate_salt()
+        self.hashed_password = get_password_hash(self.salt + password)
+
+
 class User(BaseUser):
     token: str
 
