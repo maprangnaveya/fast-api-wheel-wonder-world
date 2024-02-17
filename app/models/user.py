7084c465646194a01930ee9e5c9899e12f879cee
Naveya Jintaya <naveya@divertise.asis>
2024-02-17 21:37:18 +0700

    Create model for request and response user.

--- app/models/user.py
+++ app/models/user.py
@@ -6,3 +6,15 @@ from .read_write_model import RWModel
 class BaseUser(RWModel):
     email: EmailStr = Field(...)
     name: str = Field(alias="full_name")
+
+class User(BaseUser):
+    token: str
+
+
+class UserForLogin(RWModel):
+    email: EmailStr
+    password: str
+
+
+class UserForResponse(RWModel):
+    user: User
