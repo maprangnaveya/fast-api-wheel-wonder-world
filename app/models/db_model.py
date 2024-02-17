5091dc6e48e2158f8aaa61d67643f3505e5c8990
Naveya Jintaya <naveya@divertise.asis>
2024-02-17 22:24:13 +0700

    Create base class db model.

---
+++ app/models/db_model.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import Annotated, Optional
+
+from pydantic import BaseModel, BeforeValidator, Field
+
+# Represents an ObjectId field in the database.
+# It will be represented as a `str` on the model so that it can be serialized to JSON.
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+
+class DateTimeModelMixin(BaseModel):
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+
+
+class DBModelMixin(DateTimeModelMixin):
+    # The primary key for the UserModel, stored as a `str` on the instance.
+    # This will be aliased to `_id` when sent to MongoDB,
+    # but provided as `id` in the API requests and responses.
+    # set `id` default value to `None`, so MongoDB will auto generate `id` for us? TODO:
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
