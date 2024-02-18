7b609978897367c02415da1f9324f6219487fcd9
Naveya Jintaya <naveya@divertise.asis>
2024-02-18 23:57:37 +0700

    Create fixture test_client to pass client to all test cases.

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,11 +2,9 @@ from fastapi import status
 from fastapi.testclient import TestClient
 from main import app
 
-client = TestClient(app)
 
-
-def test_root_returns_correct_data():
-    response = client.get("/")
+def test_root_returns_correct_data(test_client):
+    response = test_client.get("/")
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "Hello World! I'm Wheel Wonder World API"}
